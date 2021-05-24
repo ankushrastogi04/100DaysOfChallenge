6e47af9fdb38c405bcd414bc83f81684db2e3b7b
Ankush Rastogi <ankushrastogi04@gmail.com>
2021-05-23 21:47:23 -0500

    Update LeetCodePython.py

--- LeetCodePython.py
+++ LeetCodePython.py
@@ -99,6 +99,39 @@ class Solution:
         return(lst)  
          
     
+# You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+# Example 1:
+# Input: jewels = "aA", stones = "aAAbbbb"
+# Output: 3    
     
     
+    class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        cnt=0
+        for i in jewels:
+            for j in stones:
+                if i == j:
+                    cnt=cnt+1
+        return(cnt)            
+     
+        
+        
+# You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+# Given the string command, return the Goal Parser's interpretation of command.
+
+# Example 1:
+
+# Input: command = "G()(al)"
+# Output: "Goal"
+# Explanation: The Goal Parser interprets the command as follows:
+# G -> G
+# () -> o
+# (al) -> al
+# The final concatenated result is "Goal".
     
+    class Solution:
+    def interpret(self, command: str) -> str:
+        return(command.replace("()","o").replace("(al)","al"))
+        #return(finalCommand)
